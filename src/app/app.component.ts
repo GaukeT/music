4ebb209a7779f4d165c16a814de752eb3e4d09af
GaukeT <gauket.dev@gmail.com>
2022-08-05 13:59:22 +0200

    initial start with calculating major key chords

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'music';
+export class AppComponent implements OnInit {
+  title = 'Music';
+  selected: string = 'C';
+
+  useSharps: boolean = false;
+  sharps: string[] = ['A','A#','B','C','C#','D','D#','E','F','F#','G','G#'];
+  flats: string[] = ['A','B♭','B','C','D♭','D','E♭','E','F','G♭','G','A♭'];
+  notes: string[] = [];
+  chords: string[] = [];
+  
+  ngOnInit(): void {
+    this.setUseSharps();
+    this.calculateKey(this.selected);
+  }
+
+  setUseSharps(): void {
+    this.useSharps = !this.useSharps;
+    this.notes = this.useSharps ? this.sharps : this.flats;
+    this.setValue(this.selected);
+  }
+
+  setValue(value: string): void {
+    this.selected = value;
+    this.calculateKey(value);
+  }
+  
+  calculateKey(value: string): void {
+    this.chords = [];
+    let index = this.notes.indexOf(value);
+    this.chords[0] = this.notes[index];
+    this.chords[1] = this.notes[(index + 2) % 12] + 'm';
+    this.chords[2] = this.notes[(index + 4) % 12]  + 'm';
+    this.chords[3] = this.notes[(index + 5) % 12];
+    this.chords[4] = this.notes[(index + 7) % 12];
+    this.chords[5] = this.notes[(index + 9) % 12] + 'm';
+    this.chords[6] = this.notes[(index + 11) % 12] + '°';
+  }
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatTableModule } from '@angular/material/table';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatButtonToggleModule,
+    MatTableModule,
+    MatSlideToggleModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
